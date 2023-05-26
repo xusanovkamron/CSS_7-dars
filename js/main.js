3d918020f487298ec5f4eb972f30e69f2758e64d
dastgddg11wregds <samandarkomiljonov24@gmail.com>
2023-05-26 20:07:10 +0500

    CSS 7-dars
    Yukland

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap");
+let byFormGroup = document.getElementsByTagName("div").namedItem("form-group");
+let bylabel1 = document.getElementsByTagName("label").namedItem("label1");
+let bylabel2 = document.getElementsByTagName("label").namedItem("label2");
+let bylabel3 = document.getElementsByTagName("label").namedItem("label3");
+let bylabel4 = document.getElementsByTagName("label").namedItem("label4");
+let bylabel5 = document.getElementsByTagName("label").namedItem("label5");
+let bylabel6 = document.getElementsByTagName("label").namedItem("label6");
+
+let byInput1 = document.getElementsByTagName("input").namedItem("firstName");
+let byInput2 = document.getElementsByTagName("input").namedItem("lastName");
+let byInput3 = document.getElementsByTagName("input").namedItem("email");
+let byInput4 = document.getElementsByTagName("input").namedItem("premail");
+let byInput5 = document.getElementsByTagName("input").namedItem("password");
+
+byWrap.style.width = "1200px";
+byWrap.style.margin = "0 auto";
+
+byFormGroup.style.width = "500px";
+byFormGroup.style.margin = "50px auto";
+byFormGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+byFormGroup.style.padding = "1rem 1.2rem";
+byFormGroup.style.boxSizing = "border-box";
